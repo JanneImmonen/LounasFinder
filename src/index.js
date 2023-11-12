f6e33c32a532edd48e88c9ce0a207bab39493c20
Janne Immonen <immonenjanne@icloud.com>
2023-11-12 13:33:36 +0200

    Updates

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS globally
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import any global contexts if you have them
+// import { MyGlobalContextProvider } from './context/MyGlobalContext';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // Wrap <App /> in providers as necessary
+  // <MyGlobalContextProvider>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  // </MyGlobalContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,18 @@
 // src/pages/Home.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/Home.css'; // Ensure you have the Home.css file with styles
 
 const Home = () => {
   return (
-    <div className="home-container">
-      <h1>Welcome to LounasFinder</h1>
-      <p>Discover and share the best lunch spots in town.</p>
+    <div className="home-container text-center">
+      <h1 className="display-3">Welcome to LounasFinder</h1>
+      <p className="lead">Discover and share the best lunch spots in town.</p>
+      <hr className="my-4" />
+      <p>Join now and start exploring the best places to eat around you.</p>
       <div className="auth-links">
-        <Link to="/signin" className="btn btn-primary">Sign In</Link>
-        <Link to="/signup" className="btn btn-secondary">Sign Up</Link>
+        <Link to="/signin" className="btn btn-primary btn-lg m-2">Sign In</Link>
+        <Link to="/signup" className="btn btn-secondary btn-lg m-2">Sign Up</Link>
       </div>
     </div>
   );
